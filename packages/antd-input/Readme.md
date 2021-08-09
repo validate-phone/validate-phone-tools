5c903f90863d125f4b65dc53cd329ffbae3a4a18
Denes PAL <dsdenes@gmail.com>
2021-08-09 20:00:21 +0200

    Add antd-demo

---
+++ packages/antd-input/Readme.md
@@ -0,0 +1,24 @@
+# Validate phone numbers for web
+
+- <1Kb
+- Zero-dependency
+- Utilizes validatephonenumber.com free API
+- Compatible with all modern browsers
+
+## How to use
+
+### ES6
+
+```ts
+import { isValidPhoneNumber } from '@validate-phone/web'
+
+const isValid = await validatePhoneNumber('+123456789')
+```
+
+### CommonJS
+
+```js
+const { isValidPhoneNumber } = require('@validate-phone/web')
+
+const isValid = await validatePhoneNumber('+123456789')
+```
---
+++ packages/antd-input/src/index.tsx
@@ -0,0 +1,43 @@
+import { isValidPhoneNumber } from '@validate-phone/web'
+import { Form, FormItemProps, Input, InputProps } from 'antd'
+import { NamePath, Rule, RuleRender } from 'rc-field-form/lib/interface'
+import React, { useCallback, useMemo } from 'react'
+
+interface PhoneInputProps extends InputProps {}
+
+export const PhoneInput: React.FC<PhoneInputProps> = (props) => {
+  return <Input {...props} />
+}
+
+export interface PhoneInputFormItemProps extends FormItemProps {
+  name: NamePath
+  skipValidityCheck?: boolean
+  errorMessage?: string
+}
+
+export const PhoneInputFormItem: React.FC<PhoneInputFormItemProps> = (props) => {
+  const validatePhoneNumberRule: RuleRender = useCallback(() => {
+    return {
+      validator: async (_, value) => {
+        if (props.skipValidityCheck) {
+          return
+        }
+        if ((await isValidPhoneNumber(value)) !== true) {
+          throw new Error(props.errorMessage ? props.errorMessage : 'Incorrect phone number')
+        }
+      },
+      type: 'string'
+    }
+  }, [])
+
+  const rules = useMemo(
+    () => [validatePhoneNumberRule as Rule].concat(props.rules ? props.rules : []),
+    [props.rules, validatePhoneNumberRule]
+  )
+
+  return (
+    <Form.Item {...props} rules={rules}>
+      {props.children}
+    </Form.Item>
+  )
+}
